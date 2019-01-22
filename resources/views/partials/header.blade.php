dd94f77a3dc1ca9ff4904ce5b363cbfd27f875ee
calebanthony <caleb.m.anthony@gmail.com>
2019-01-22 16:01:23 -0700

    Add Justin's Instagram to the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,7 @@
     </a>
     <div id="header-profiles" class="buttons is-2 is-hidden-mobile">
       <a href="https://www.instagram.com/itsbrittanystuart/" class="has-text-grey-dark"><span class="fab fa-instagram fa-fw fa-2x"></span></a>
+      <a href="https://www.instagram.com/itsjustinstuart/" class="has-text-grey-dark"><span class="fab fa-instagram fa-fw fa-2x"></span></a>
     </div>
     <div id="header-menu" class="column is-6 is-offset-3" role="navigation">
       @if (has_nav_menu('primary_navigation'))
